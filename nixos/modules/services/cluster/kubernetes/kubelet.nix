05eb0cfb3e6e4c82d6ef55c7f7171f8cce7697a7
Edwin Mackenzie-Owen <edwin.mowen@gmail.com>
2022-10-02 18:40:17 +0200

    fix prestart

--- nixos/modules/services/cluster/kubernetes/kubelet.nix
+++ nixos/modules/services/cluster/kubernetes/kubelet.nix
@@ -295,6 +295,7 @@ in
           '') cfg.seedDockerImages}
 
           rm /opt/cni/bin/* || true
+          mkdir -p ${cfg.cni.configDir} || true
           ${concatMapStrings (package: ''
             echo "Linking cni package: ${package}"
             ln -fs ${package}/bin/* /opt/cni/bin
@@ -306,7 +307,6 @@ in
           MemoryAccounting = true;
           Restart = "on-failure";
           RestartSec = "1000ms";
-          ExecStartPre = lib.mkIf (!cfg.cni.manageConfigDir) ''mkdir -p ${cfg.cni.configDir}'';
           ExecStart = ''${top.package}/bin/kubelet \
             --address=${cfg.address} \
             --authentication-token-webhook \
