82c40ac6dfd3426a850114660a230ddf01acd3ef
Edwin Mackenzie-Owen <edwin.mowen@gmail.com>
2024-02-14 12:15:37 +0100

    socket_vmnet: init at 1.1.3

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -2569,6 +2569,10 @@ with pkgs;
     inherit (darwin.apple_sdk.frameworks) Security;
   };
 
+  socket_vmnet = callPackage ../os-specific/darwin/socket_vmnet {
+    inherit (darwin.apple_sdk.frameworks) vmnet;
+  };
+
   steampipePackages = recurseIntoAttrs (
     callPackage ../tools/misc/steampipe-packages { }
   );
